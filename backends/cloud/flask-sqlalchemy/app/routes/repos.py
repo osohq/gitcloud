c3bead7e36edfe0355060115bafc2d98ee6c5e1a
Gabe Jackson <17556281+gj@users.noreply.github.com>
2022-05-18 07:59:27 -0400

    Repos

--- backends/cloud/flask-sqlalchemy/app/routes/repos.py
+++ backends/cloud/flask-sqlalchemy/app/routes/repos.py
@@ -1,33 +1,44 @@
 from flask import Blueprint, g, request, jsonify
+from werkzeug.exceptions import Forbidden, NotFound
 
 from ..models import Org, Repo
+from .helpers import oso
 
 bp = Blueprint("routes.repos", __name__, url_prefix="/orgs/<int:org_id>/repos")
 
 
-# docs: begin-repo-index
 @bp.route("", methods=["GET"])
-# @session({Org: "list_repos", Repo: "read"})
 def index(org_id):
     org = g.session.get_or_404(Org, id=org_id)
-    repos = g.session.query(Repo).filter_by(org_id=org_id)
-    return jsonify([repo.repr() for repo in repos])
-    # docs: end-repo-index
+    if not oso.authorize(g.current_user, "list_repos", org):
+        raise Forbidden
+    authorized_ids = oso.list(g.current_user, "read", "Repo")
+    if authorized_ids[0] == "*":
+        repos = g.session.query(Repo).filter_by(org_id=org_id)
+        return jsonify([r.repr() for r in repos])
+    else:
+        repos = g.session.query(Repo).filter(
+            Repo.id.in_(authorized_ids), Repo.org_id == org_id
+        )
+        return jsonify([r.repr() for r in repos])
 
 
 @bp.route("", methods=["POST"])
-# @session({Org: "create_repos", Repo: "read"})
 def create(org_id):
     payload = request.get_json(force=True)
     org = g.session.get_or_404(Org, id=org_id)
-    repo = Repo(name=payload.get("name"), org=org)
+    if not oso.authorize(g.current_user, "create_repos", org):
+        raise Forbidden
+    repo = Repo(name=payload["name"], org=org)
     g.session.add(repo)
     g.session.commit()
+    oso.tell("has_relation", repo, "parent", org)
     return repo.repr(), 201
 
 
 @bp.route("/<int:repo_id>", methods=["GET"])
-# @session({Repo: "read"})
 def show(org_id, repo_id):
     repo = g.session.get_or_404(Repo, id=repo_id)
+    if not oso.authorize(g.current_user, "read", repo):
+        raise NotFound
     return repo.repr()
