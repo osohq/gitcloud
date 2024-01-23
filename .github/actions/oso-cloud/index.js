f5eaec130add07b238e782b7fd998a7f3a9e54a6
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2024-01-23 15:33:08 -0500

    Fine wrap it in a function then.

--- .github/actions/oso-cloud/index.js
+++ .github/actions/oso-cloud/index.js
@@ -2,49 +2,53 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
 
-console.log(`Installing Oso Cloud CLI`);
+async function installOsoCloudCLI() {
+    console.log(`Installing Oso Cloud CLI`);
 
-let curlOutput = '';
-let curlError = '';
-let catOutput = '';
-let catError = '';
+    let curlOutput = '';
+    let curlError = '';
+    let catOutput = '';
+    let catError = '';
 
-const curlOptions = {};
-const catOptions = {};
+    const curlOptions = {};
+    const catOptions = {};
 
-curlOptions.listeners = {
-  stdout: (data) => {
-    curlOutput += data.toString();
-  },
-  stderr: (data) => {
-    curlError += data.toString();
-  }
-};
+    curlOptions.listeners = {
+      stdout: (data) => {
+        curlOutput += data.toString();
+      },
+      stderr: (data) => {
+        curlError += data.toString();
+      }
+    };
 
-catOptions.listeners = {
-  stdout: (data) => {
-    catOutput += data.toString();
-  },
-  stderr: (data) => {
-    catError += data.toString();
-  }
-};
+    catOptions.listeners = {
+      stdout: (data) => {
+        catOutput += data.toString();
+      },
+      stderr: (data) => {
+        catError += data.toString();
+      }
+    };
 
-await exec.exec('curl', ['-L', 'https://cloud.osohq.com/install.sh'], curlOptions)
-await exec.exec('cat', ['./install.sh'], catOptions)
+    await exec.exec('curl', ['-L', 'https://cloud.osohq.com/install.sh'], curlOptions)
+    await exec.exec('cat', ['./install.sh'], catOptions)
 
-console.log(`curl stdout:`)
-console.log(curlOutput)
-console.log(`curl stderr:`)
-console.log(curlError)
+    console.log(`curl stdout:`)
+    console.log(curlOutput)
+    console.log(`curl stderr:`)
+    console.log(curlError)
 
-console.log(`cat stdout:`)
-console.log(catOutput)
-console.log(`cat stderr:`)
-console.log(catError)
+    console.log(`cat stdout:`)
+    console.log(catOutput)
+    console.log(`cat stderr:`)
+    console.log(catError)
 
-core.setOutput("version", '1.0');
+    core.setOutput("version", '1.0');
 
-// Get the JSON webhook payload for the event that triggered the workflow
-const payload = JSON.stringify(github.context.payload, undefined, 2)
-console.log(`The event payload: ${payload}`);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+}
+
+installOsoCloudCLI();
