c4d4519ffd4d0ed2771fee051003f47832cfa565
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2024-01-23 15:23:33 -0500

    Remove async commands from try/catch block. Capture stdout and stderr

--- .github/actions/oso-cloud/index.js
+++ .github/actions/oso-cloud/index.js
@@ -2,16 +2,49 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
 
-try {
-  console.log(`Installing Oso Cloud CLI`);
-  await exec.exec('curl', ['-L', 'https://cloud.osohq.com/install.sh'])
-  await exec.exec('cat', ['./install.sh'])
-  
-  core.setOutput("version", '1.0');
-  
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+console.log(`Installing Oso Cloud CLI`);
+
+let curlOutput = '';
+let curlError = '';
+let catOutput = '';
+let catError = '';
+
+const curlOptions = {};
+const catOptions = {};
+
+curlOptions.listeners = {
+  stdout: (data) => {
+    curlOutput += data.toString();
+  },
+  stderr: (data) => {
+    curlError += data.toString();
+  }
+};
+
+catOptions.listeners = {
+  stdout: (data) => {
+    catOutput += data.toString();
+  },
+  stderr: (data) => {
+    catError += data.toString();
+  }
+};
+
+await exec.exec('curl', ['-L', 'https://cloud.osohq.com/install.sh'], curlOptions)
+await exec.exec('cat', ['./install.sh'], catOptions)
+
+console.log(`curl stdout:`)
+console.log(curlOutput)
+console.log(`curl stderr:`)
+console.log(curlError)
+
+console.log(`cat stdout:`)
+console.log(catOutput)
+console.log(`cat stderr:`)
+console.log(catError)
+
+core.setOutput("version", '1.0');
+
+// Get the JSON webhook payload for the event that triggered the workflow
+const payload = JSON.stringify(github.context.payload, undefined, 2)
+console.log(`The event payload: ${payload}`);
