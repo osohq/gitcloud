37cbe34aac74aacf88973ca4458b14557c3cd721
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2024-01-24 17:27:23 -0500

    Make it async

--- .github/actions/oso/install-oso-cloud-local-binary/index.js
+++ .github/actions/oso/install-oso-cloud-local-binary/index.js
@@ -8,37 +8,37 @@ async function installOsoCloudLocalBinary() {
 
     let stdout = '';
     let stderr = '';
-    const options = {};
-
-    options.listeners = {
-      stdout: (data) => {
-        stdout += data.toString();
-      },
-      stderr: (data) => {
-        stderr += data.toString();
-      }
-    };
 
     cmds = [];
     cmds.push(['curl', ['-L', 'https://oso-local-development-binary.s3.amazonaws.com/latest/oso-local-development-binary-linux-x86_64.tar.gz']]);
     cmds.push(['tar', ['-xvzf', './oso-local-development-binary-linux-x86_64.tar.gz']]);
     cmds.push(['chmod', ['0700', './standalone']] );
 
-    cmds.forEach(item => {
-      stdout = '';
-      stderr = '';
+    for(const item of cmds) {
+      let stdout = '';
+      let stderr = '';
+      const options = {};
 
-      cmd = item[0];
-      args = item[1];
+      options.listeners = {
+        stdout: (data) => {
+          stdout += data.toString();
+        },
+        stderr: (data) => {
+          stderr += data.toString();
+        }
+      };
 
-      console.log(`cmd: ${cmd}, args: ${args}}`)
-//      await exec.exec(cmd, args, options)
+      const cmd = item[0];
+      const args = item[1];
+
+      console.log(`cmd: ${cmd}, args: ${args}`);
+      await exec.exec(cmd, args, options);
 
 //      console.log(`stdout:`)
 //      console.log(stdout)
 //      console.log(`stderr:`)
 //      console.log(stderr)
-    })
+    }
 
 
 
