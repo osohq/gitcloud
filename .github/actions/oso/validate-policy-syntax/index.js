a74745b6220fd3a13cac15c8b2008427b7d14419
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2024-01-25 17:59:34 -0500

    More cleanup

--- .github/actions/oso/validate-policy-syntax/index.js
+++ .github/actions/oso/validate-policy-syntax/index.js
@@ -1,11 +1,9 @@
-const core = require('@actions/core');
 const exec = require('@actions/exec');
-const path = require('path');
 const fs = require('fs');
 const { globSync } = require('glob');
 
 async function validatePolicySyntax() {
-  console.log(`Installing Oso Cloud local binary`);
+  console.log('Validating .polar file syntax');
 
   let stdout = '';
   let stderr = '';
@@ -23,11 +21,6 @@ async function validatePolicySyntax() {
 
   const polarFiles = globSync('**/*.polar', { ignore: 'node_modules/**' }).filter((file) => !fs.lstatSync(file).isSymbolicLink());
   await exec.exec('oso-cloud', ['validate', polarFiles.join(" ")], options);
-
-  console.log(`stdout:`)
-  console.log(stdout)
-  console.log(`stderr:`)
-  console.log(stderr)
 }
 
 validatePolicySyntax();
