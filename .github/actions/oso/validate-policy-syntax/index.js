4e139bd52cf138cbe2c19ef94ba25616ccd1341e
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2024-01-25 17:56:39 -0500

    Clean up syntax validation

--- .github/actions/oso/validate-policy-syntax/index.js
+++ .github/actions/oso/validate-policy-syntax/index.js
@@ -5,46 +5,29 @@ const fs = require('fs');
 const { globSync } = require('glob');
 
 async function validatePolicySyntax() {
-    console.log(`Installing Oso Cloud local binary`);
+  console.log(`Installing Oso Cloud local binary`);
 
+  let stdout = '';
+  let stderr = '';
 
-    let stdout = '';
-    let stderr = '';
+  const options = {};
 
-    const options = {};
-
-    options.listeners = {
-      stdout: (data) => {
-        stdout += data.toString();
-      },
-      stderr: (data) => {
-        stderr += data.toString();
-      }
-    };
-
-    const polarFiles = globSync('**/*.polar', { ignore: 'node_modules/**' }).filter((file) => !fs.lstatSync(file).isSymbolicLink());
-    
-    console.log(polarFiles)
-    
-    cmds = [];
-    cmds.push(['oso-cloud', ['validate', polarFiles.join(" ")]]);
-    cmds.push(['ls']);
-
-    for(const item of cmds) {
-      stdout = '';
-      stderr = '';
-      const cmd = item[0];
-      const args = item[1];
-
-      console.log(`cmd: ${cmd}, args: ${args}`);
-      await exec.exec(cmd, args, options);
-
-      console.log(`stdout:`)
-      console.log(stdout)
-      console.log(`stderr:`)
-      console.log(stderr)
-
+  options.listeners = {
+    stdout: (data) => {
+      stdout += data.toString();
+    },
+    stderr: (data) => {
+      stderr += data.toString();
     }
+  };
+
+  const polarFiles = globSync('**/*.polar', { ignore: 'node_modules/**' }).filter((file) => !fs.lstatSync(file).isSymbolicLink());
+  await exec.exec('oso-cloud', ['validate', polarFiles.join(" ")], options);
+
+  console.log(`stdout:`)
+  console.log(stdout)
+  console.log(`stderr:`)
+  console.log(stderr)
 }
 
 validatePolicySyntax();
