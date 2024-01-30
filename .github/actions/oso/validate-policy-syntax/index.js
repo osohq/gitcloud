c9dcc4991ad67d5823d0bce5cdc0720fc6db5431
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2024-01-30 11:51:09 -0500

    Concatenate arrays

--- .github/actions/oso/validate-policy-syntax/index.js
+++ .github/actions/oso/validate-policy-syntax/index.js
@@ -22,7 +22,7 @@ async function validatePolicySyntax() {
   //const polarFiles = globSync('**/*.polar', { ignore: 'node_modules/**' }).filter((file) => !fs.lstatSync(file).isSymbolicLink());
   const polarFiles = await glob('**/*.polar', { ignore: 'node_modules/**' })
   const polarFilesNoSymlinks = polarFiles.filter((file) => !fs.lstatSync(file).isSymbolicLink());
-  await exec.exec('oso-cloud', ['validate', polarFilesNoSymlinks.join(" ")], options);
+  await exec.exec('oso-cloud', ['validate'].concat(polarFilesNoSymlinks), options);
 }
 
 validatePolicySyntax();
